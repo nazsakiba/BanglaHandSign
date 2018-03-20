8f063fb8df7dd1817f658070d1fe3953e24ce3d6
Nazmus Sakiba <nazsakiba@gmail.com>
2018-03-20 11:43:04 -0500

    All files are uploaded

---
+++ rere.m
@@ -0,0 +1,15 @@
+function p = rere( oknum )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+for cnt = 1:10
+if oknum == 1  
+[cdata] = imread('11.jpg');
+h=msgbox('Operation Completed',...
+         'Success','custom',cdata);
+
+    end
+end
+end
+
